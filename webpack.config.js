df91f1937657ac4b69216da216fcfd63cd9685df
zertu <290055513@qq.com>
2016-12-12 22:23:16 +0800

    ejs模版分头和身子

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+ 
+module.exports = {
+    //插件项
+    plugins: [commonsPlugin],
+    //页面入口文件配置
+    entry: {
+        index : './views/index.ejs'
+    },
+    //入口文件输出配置
+    output: {
+        path: 'dist/js',
+        filename: '[name].js'
+    },
+    module: {
+        //加载器配置
+        loaders: [
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+        ]
+    },
+    //其它解决方案配置
+    resolve: {
+        extensions: ['', '.js', '.json', '.scss'],
+        alias: {
+        }
+    }
+};
