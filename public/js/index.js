f85db13d998b0258d9473207c3a5107f4389a959
zertu <290055513@qq.com>
2016-12-12 22:41:28 +0800

    环境配置

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+onload=()=>{
+    const trianglify = require('trianglify')
+let pattern = trianglify({
+    height: window.innerHeight,
+    width: window.innerWidth,
+    cell_size: 450,
+    x_colors: ['#66CCFF', '#99CCFF', '#99FFFF'],
+    y_colors: ['#339900', '#33CC99', '#33FFCC']
+})
+    pattern.canvas(document.getElementById('bg'))
+}
+let data={
+    width:window.innerWidth,
+    height: window.innerHeight
+}
