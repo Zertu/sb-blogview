f62e3819be6af87b12ccdec5c276285285232286
zertu <290055513@qq.com>
2016-12-13 21:34:46 +0800

    sql连接函数

---
+++ routes/sql.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql')
+const pool = mysql.createPool({
+    host: '104.128.86.50',
+    user: 'root',
+    password: 'a7864548',
+    database: 'sbblog',
+    port: 3306,
+})
+/**
+ * 
+ * 
+ * @param {any} sql字符串
+ * @param {any} 回调函数，有err和rows参数
+ */
+function querysql(sqlstring,fn) {
+    pool.getConnection((err,con)=>{
+        if(err){
+            console.error(err)
+        }else{
+            con.query(sqlstring,fn(err,rows))
+        }
+    })
+}
