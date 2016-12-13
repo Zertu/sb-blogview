3e6c04f4c8e2bb4f21dfdb121f5200458e4793e6
zertu <290055513@qq.com>
2016-12-13 23:00:15 +0800

    登录接口

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const router = express.Router()
-const user = require('./user')
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'sb-blog' });
+const express = require('express'),
+  router = express.Router(),
+  user = require('./user'),
+  session = require('express-session')
+  /* GET home page. */
+let app= express()
+app.use(session({
+    secret: 'mysbblog', // 建议使用 128 个字符的随机字符串
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }
+}))
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'sb-blog'
+  });
 });
-router.get('/article',(req,res)=>{
-  res.render('article',{ title: 'sb-blog' })
+router.get('/article',(req, res) => {
+  res.render('article', {
+    title: 'sb-blog',session:req.session
+  })
 })
 
 
-router.post('/login',(req,res)=>{
-  user.checklogin(req,res)
+router.post('/login', (req, res) => {
+  user.checklogin(req, res)
 })
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,15 @@
 const sql = require('./sql')
-/**
- * 
- * 
- * @param {any} req 请求头
- * @param {any} res 相应头
- */
-function checklogin(req,res) {
-    let body =req.body
+    /**
+     * 
+     * 
+     * @param {any} req 请求头
+     * @param {any} res 相应头
+     */
+function checklogin(req, res) {
+    let body = req.body
+
     res.json(body)
 }
-module.exports={
-    checklogin:checklogin
+module.exports = {
+    checklogin: checklogin
 }
