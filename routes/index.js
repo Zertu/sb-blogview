704d83fb755471230d74de2fa7fef761bb35eb34
zertu <290055513@qq.com>
2016-12-14 10:33:34 +0800

    头像位置

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,31 @@ router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'sb-blog'
   });
-});
-router.get('/article', (req, res) => {
+})
 
-  console.log(req)
+router.get('/article', (req, res) => {
   res.render('article', {
     title: 'sb-blog',
     session: req.session.user
   })
 })
 
+router.get('/logout',(req,res)=>{
+  req.session.user=null
+  res.render('article',{
+    title:'sb-blog',
+    session:req.session.user
+  })
+})
 
+// post 请求处理
 router.post('/login', (req, res) => {
+  if(req.session.user){
+    res.render('article', {
+      title: 'sb-blog',
+      session: req.session.user
+    })
+  }
   let row = user.checklogin(req, res, function (rows) {
     res.render('article', {
       title: 'sb-blog',
